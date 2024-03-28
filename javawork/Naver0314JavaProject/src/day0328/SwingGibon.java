e2a4df0a23a293eb9adb0e6a0f86e5b7d9b4f432
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-03-28 12:43:21 +0900

    3월 28일 수업

---
+++ javawork/Naver0314JavaProject/src/day0328/SwingGibon.java
@@ -0,0 +1,29 @@
+package day0328;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class SwingGibon extends JFrame {
+	
+	public SwingGibon(String title) {
+		super(title);
+		this.setLocation(100, 100); // 시작위치
+		this.setSize(300, 300); // 창 크기
+		this.getContentPane().setBackground(new Color(93, 199, 187)); // 배경색 변경
+		//this.getContentPane().setBackground(Color.yellow);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프레임 종료 (이 코드를 넣어야 프레임 창을 종료할 때 컴파일러도 종료된다)
+		this.initDesign(); // 각종 컴포넌트 생성
+		this.setVisible(true); // 보이게 하기
+	}
+	
+	public void initDesign() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingGibon a = new SwingGibon("기본창");
+	}
+
+}
